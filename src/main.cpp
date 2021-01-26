3a85ddabf20bdb81a905b8c15c87b36672f75d72
Alexey Kreshchuk <akrsch@gmail.com>
2021-01-26 12:00:51 +0300

    seminar 3@202

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "lib.hpp"
+#include <array>
 #include <fmt/core.h>
-#include <fmt/ranges.h>
 #include <fmt/ostream.h>
+#include <fmt/ranges.h>
 #include <initializer_list>
 #include <iostream>
 #include <memory>
 #include <ostream>
+#include <stdexcept>
 #include <vector>
 
 /*
@@ -55,13 +57,19 @@ struct Stack {
     size_t top = 0;
 
     auto push_back(int value) {
+        if (top >= max_number_of_elements)
+            throw std::out_of_range("stack is full"); // raise в python
         elements[top] = value;
         ++top;
+        // elements[top++] = value;
     }
 
     auto pop_back() {
+        if (top == 0)
+            throw std::out_of_range("stack is empty");
         --top;
         return elements[top];
+        // return elements[--top];
     }
 
     Stack() = default;
@@ -69,6 +77,11 @@ struct Stack {
     Stack(std::initializer_list<int> list) {
         for (auto elem : list)
             push_back(elem);
+    }
+    int operator[](size_t index) const {
+        if (index < top)
+            return elements[top - index];
+        throw std::out_of_range("index too large");
     }
 };
 std::ostream &operator<<(std::ostream &stream, Stack const &stack) {
@@ -86,11 +99,42 @@ auto step3() {
     // элементы в {} рассматриваются как отдельные аргументы конструктора
     std::cout << s2 << "\n";
     fmt::print("{}\n", s1);
+    try {
+        s1.pop_back();
+    } catch (std::out_of_range const &error) {
+        fmt::print(std::cerr, "got error: {}\n", error.what());
+        // throw; // как raise
+    }
+    fmt::print("s2[1]: {}\n", s2[1]);
+}
+
+struct example1 {
+    int v;
+    explicit example1(int value) { v = value; }
+    explicit example1(std::initializer_list<int> value) {
+        v = int(value.size());
+    }
+    explicit example1(std::initializer_list<int> value,
+                      std::initializer_list<int> /*unused*/) {
+        v = int(value.size());
+    }
+};
+
+auto step4() {
+    auto e1 = example1(2);
+    auto e2 = example1{2};
+    // Компилятор пытается превратить example1{2} в
+    // example1(std::initializer_list{2})
+    auto e3 = example1({2}, {3});
+    fmt::print("e1 = {}, e2 = {}, e3 = {}\n", e1.v, e2.v, e3.v);
 }
 
 int main() {
     std::cout << fmt::format("not_random number is {0:04}\n", not_random());
     fmt::print("Another string and a vector {}\n", std::vector{1, 2, 3, 4});
+    step1();
+    step2();
     step3();
+    step4();
     return 0;
 }
