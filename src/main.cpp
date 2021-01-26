2e93949bfc52c10322fa7ee560ef4abf1e325561
Alexey Kreshchuk <akrsch@gmail.com>
2021-01-26 09:55:08 +0300

    add fmt

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include "lib.hpp"
+#include <fmt/core.h>
+#include <fmt/ranges.h>
 #include <iostream>
+#include <vector>
 
 int main() {
-  std::cout << not_random() << "\n";
-  return 0;
+    std::cout << fmt::format("not_random number is {0:04}\n", not_random());
+    fmt::print("Another string and a vector {}\n", std::vector{1, 2, 3, 4});
+
+    return 0;
 }
