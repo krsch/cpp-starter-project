06ad53cd1c923b3addc8143fbe26507b266251fa
Alexey Kreshchuk <akrsch@gmail.com>
2021-02-09 16:02:54 +0300

    lambda and iterator

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
 #include "lib.hpp"
-<<<<<<< HEAD
-
-int main() { return 0; }
-=======
 #include <algorithm>
 #include <fmt/core.h>
+#include <fmt/ranges.h>
 #include <iostream>
+#include <list>
 #include <memory>
 #include <vector>
 
@@ -59,6 +57,41 @@ auto step02() {
     auto vec = std::vector{1, 2, 3, 4};
     auto sm4 = std::all_of(vec.begin(), vec.end(), smaller_than_4);
     fmt::print("All is <4: {}\n", sm4);
+    fmt::print("All positive: {}\n",
+               std::all_of(vec.begin(), vec.end(),
+                           [](int value) { return value > 0; }));
+    auto N = 3;
+    fmt::print("All not equal to {}: {}\n", N,
+               std::all_of(vec.begin(), vec.end(),
+                           [N](int value) { return value != N; }));
+    // [N] - скопировать
+    // [&N] - сохранить ссылку
+    // [&] - сохранить сслыки на всё
+    // [b = N] - скопировать N под именем b
+    // [b = N*N] - создать переменную b со значением N*N
+    // [=] - скопировать всё (нужно ли?)
+    // [this] - скопировать указатель this (взять ссылку на текущий экземпляр
+    //          класса)
+    // auto l = [b=[](){return 1;}]() {return b();}; // так тоже работает
+}
+
+auto step03() {
+    auto vec = std::vector{1, 2, 3, 4};
+    // std::vector<int>::iterator
+    auto it = std::find_if(vec.begin(), vec.end(),
+                           [](int x) { return x * x > x + x; });
+    // it указывает на 3
+    --it; // Двигаемся назад, it указывает на 2
+    it = vec.erase(it); // 2 удалено, it указывает на 3
+    fmt::print("After erase: {}, it points to {} at pos {}\n", vec, *it,
+               it - vec.begin());
+    auto l = std::list{1, 2, 3, 4};
+    for (auto lit = l.begin(); lit != l.end();)
+        if (*lit % 2 == 0)
+            lit = l.erase(lit);
+        else
+            ++lit;
+    fmt::print("After loop with erase {}\n", l);
 }
 
 int main() {
@@ -67,6 +100,6 @@ int main() {
   l = make_listx3(1, 2, 3);
   l = make_list(1, 2, 3);
   step02();
+  step03();
   return 0;
 }
->>>>>>> template
