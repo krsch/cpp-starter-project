610483ed785187c7c811a4fd6ad4d5ab544cfedb
Alexey Kreshchuk <akrsch@gmail.com>
2021-03-02 12:34:23 +0300

    make list bidirectional

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,10 @@
 #include <cctype>
 #include <initializer_list>
 #include <iostream>
+#include <iterator>
 #include <memory>
 #include <string_view>
+#include <fmt/ranges.h>
 
 struct ListNode {
     int value;
@@ -15,23 +17,25 @@ struct ListNode {
 
 struct List {
     std::unique_ptr<ListNode> first;
+    ListNode *last = nullptr;
     List(std::initializer_list<int> list) {
         if (list.size() == 0)
             return;
         auto it = list.begin();
         first = std::make_unique<ListNode>(*it);
-        auto node = first.get();
+        last = first.get();
         ++it;
         while (it != list.end()) {
-            node->next = std::make_unique<ListNode>(*it);
-            node->next->prev = node;
-            node = node->next.get();
+            last->next = std::make_unique<ListNode>(*it);
+            last->next->prev = last;
+            last = last->next.get();
             ++it;
         }
     }
 
-    struct iterator {
+    struct iterator : std::iterator<std::bidirectional_iterator_tag, int> {
         ListNode *node;
+        ListNode *prev;
 
         auto operator==(iterator const &other) const {
             return node == other.node;
@@ -40,29 +44,32 @@ struct List {
             return node != other.node;
         }
         auto operator++() -> iterator & { // ++a
+            prev = node;
             node = node->next.get();
             return *this;
         }
         auto operator++(int) -> iterator { // a++
             auto *cur = node;
             node = node->next.get();
-            return iterator{cur};
+            return iterator{{}, cur, cur->prev};
         }
         auto operator--() -> iterator & { // --a
-            node = node->prev;
+            node = prev;
+            prev = node->prev;
             return *this;
         }
         auto operator--(int) -> iterator { // a--
             auto *cur = node;
-            node = node->prev;
-            return iterator{cur};
+            node = prev;
+            prev = node->prev;
+            return iterator{{}, cur, cur->prev};
         }
         auto operator*() const -> auto & { return node->value; }
         auto operator->() const { return &node->value; }
     };
 
-    auto begin() { return iterator{first.get()}; }
-    auto end() const { return iterator{nullptr}; }
+    auto begin() const { return iterator{{}, first.get(), nullptr}; }
+    auto end() const { return iterator{{}, nullptr, last}; }
 };
 
 int main() {
@@ -71,6 +78,7 @@ int main() {
         std::cout << value << ", ";
     std::cout << "\n";
     std::reverse(mylist.begin(), mylist.end());
+    fmt::print("reversed: {}\n", mylist);
     using namespace std::literals;
     auto str = "hello world!"s;
     for (auto word : words_range{str})
