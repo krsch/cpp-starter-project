4d2e7956f9fb726ab2418d3af03d7589258dcbd9
Alexey Kreshchuk <akrsch@gmail.com>
2021-03-23 11:50:42 +0300

    @202

--- src/pylib.cpp
+++ src/pylib.cpp
@@ -11,20 +11,22 @@ int add(int i, int j) {
     return i + j;
 }
 
-auto operator""_dec(unsigned long long d) {
-    py::object Decimal = py::module_::import("decimal").attr("Decimal");
-    return Decimal(d);
-}
-
 auto get_decimal() {
     py::object Decimal = py::module_::import("decimal").attr("Decimal");
     return Decimal(-3.1415).attr("copy_abs")();
 }
 
-auto get_decimal2() { return 123_dec; }
+auto operator""_dec(unsigned long long d) {
+    py::object Decimal = py::module_::import("decimal").attr("Decimal");
+    return Decimal(d);
+}
+
+auto get_decimal2() { return 1_dec; }
+
+auto operator""_arg(char const *str) { return py::arg(str); }
 
 struct Pet {
-    Pet(const std::string &name) : name(name) {}
+    Pet(std::string_view name, int age) : name(name), age(age) {}
     void setName(const std::string &name_) { name = name_; }
     const std::string &getName() const noexcept { return name; }
     void set(std::string_view s) { name = s; }
@@ -36,16 +38,22 @@ struct Pet {
 
 PYBIND11_MODULE(cpplib, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
+    using namespace pybind11::literals;
 
     m.def("add", &add, "A function which adds two numbers");
     m.def("pi", &get_decimal, "Return pi as decimal");
     py::class_<Pet>(m, "Pet")
-        .def(py::init<const std::string &>())
+        .def(py::init<std::string_view, int>(), py::arg("name"), "age"_a = 0)
         .def("setName", &Pet::setName)
         .def("getName", &Pet::getName)
+        .def("__str__",
+             [](const Pet &a) {
+                 return fmt::format("<cpplib.Pet named '{}' aged {}>", a.name,
+                                    a.age);
+             })
         .def("__repr__",
              [](const Pet &a) {
-                 return fmt::format("<example.Pet named '{}'>", a.name);
+                 return fmt::format("cpplib.Pet('{}', {})", a.name, a.age);
              })
         .def_readwrite("name", &Pet::name)
         .def_property(
--- try-cpp.py
+++ try-cpp.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('./build/lib')
 import cpplib
 print(f'add(2,3) returns {cpplib.add(2, 3)}')
-p = cpplib.Pet("123")
+p = cpplib.Pet("v", 12)
 print(p, p.name, p.NAME)
 p.NAME = 'JOE'
 print(p, p.NAME)
